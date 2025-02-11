a572aa573f2c92848216ab93d51287f126fcfaac
Jeric James Viernes <jericviernes06@gmail.com>
2025-02-11 22:13:16 +0800

    function: add_class() = added teacherID, removed the month

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,13 +9,25 @@ use Illuminate\Http\Request;
 class CourseController extends Controller
 {
     public function add_class(Request $request){
-        $data = $request->validate([
-            'course_name' => 'required',
-            'teacher' => 'required|string',
-            'month' => 'required',
-            'day' => 'required',
-            'time_slot' => 'required'
-        ]);
+
+        $selectedOption = $request->input('teacher');
+        [$teacherId, $firstName] = explode('|', $selectedOption);
+
+        // $data = $request->validate([
+        //     'course_name' => 'required',
+        //     'teacher' => 'required|string',
+        //     'teacher_id' => 'required',
+        //     'day' => 'required',
+        //     'time_slot' => 'required'
+        // ]);
+
+        $data = [
+            'course_name' => $request->input('course_name'),
+            'teacher' => $firstName,
+            'teacher_id' => $teacherId,
+            'day' => $request->input('day'),
+            'time_slot' => $request->input('time_slot'),
+        ];
 
         Course::create($data);
 
