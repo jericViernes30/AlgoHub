5780ba6616c2d03d03d0d58216eba6660e49f104
Jeric James Viernes <jericviernes06@gmail.com>
2025-02-13 03:49:55 +0800

    added timestamps

--- database/migrations/2024_02_29_034607_create_il_students.php
+++ database/migrations/2024_02_29_034607_create_il_students.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('contact_number');
             $table->string('email_address');
             $table->string('status');
+            $table->timestamps();
         });
     }
 
