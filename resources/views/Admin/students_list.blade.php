9aa21d79477cc17c298e68949f94af648ba622c2
Jeric James Viernes <jericviernes06@gmail.com>
2025-02-22 23:46:57 +0800

    student list changes

--- resources/views/Admin/students_list.blade.php
+++ resources/views/Admin/students_list.blade.php
@@ -139,8 +139,9 @@
         <div class="w-full flex h-screen bg-[#F2EBFB]">
             <div class="w-1/6 h-full bg-[#f9f9f9] text-sm">
                 <div class="w-full mx-auto flex gap-5 items-center mt-28">
-                    <a href="{{route('admin.dashboard')}}" class="text-[#48494b] px-5 w-full py-2">Dashboard</a>
+                    <a href="{{route('admin.dashboard')}}" class="text-[#48494b] px-5 py-2 w-full">Dashboard</a>
                 </div>
+                {{-- navigations --}}
                 <div>
                     <div onclick="courseDropdown()">
                         <a href="{{route('admin.courses')}}" class="w-full flex items-center justify-around px-5 relative hover:bg-[#F2EBFB] ">
@@ -149,19 +150,25 @@
                     </div>
 
                     <div onclick="studentsDropdown()">
-                        <div class="w-full flex items-center justify-around px-5 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
+                        <a href="{{route('admin.students')}}" class="w-full flex items-center justify-around px-5 relative bg-[#F2EBFB] border-l-4 rounded-sm border-[#833ae0] hover:cursor-pointer">
                             <p id="students_dd" class=" w-full py-2 text-[#48494b]">Students</p>
-                        </div>
+                        </a>
+                    </div>
+
+                    <div onclick="">
+                        <a href="{{route('admin.teachers_list')}}" class="w-full flex items-center justify-around px-5 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
+                            <p id="students_dd" class=" w-full py-2 text-[#48494b]">Teachers</p>
+                        </a>
                     </div>
 
                     <div onclick="scheduleDropdown()">
-                        <div class="w-full flex items-center justify-around px-5 relative py-2 bg-[#F2EBFB] border-l-4 rounded-sm border-[#833ae0] hover:cursor-pointer">
+                        <div class="w-full flex items-center justify-around px-5 relative py-2 hover:bg-[#F2EBFB] hover:cursor-pointer">
                             <p id="sched_dd" class=" w-full text-[#48494b]">Schedule</p>
                             <svg id="arrow2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="12" height="12"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
                         </div>
                         <div id="schedules" class="hidden mb-5">
                             <div class="w-full flex items-center px-10 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
-                                <a href="{{route('admin.schedule')}}" class="py-2 text-[#48494b] hover:cursor-pointer">Classes</a>
+                                <a href="{{ route('admin.schedule') }}" class="py-2 text-[#48494b] hover:cursor-pointer">Classes</a>
                             </div>
                             <div class="w-full flex items-center px-10 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
                                 <a href="{{route('admin.il_schedule')}}" class="py-2 text-[#48494b] hover:cursor-pointer">Intro Lessons</a>
@@ -173,20 +180,20 @@
                     </div>
                 </div>
             </div>
-            <div class="w-full p-4">
+            <div class="w-full p-10">
                 <div class="w-full mx-auto bg-[#f9f9f9] rounded-xl p-4 text-sm">
-                    <p class="text-lg font-medium mb-4">Walk-in clients</p>
+                    <p class="text-lg font-medium mb-4">Students List</p>
                     <div class="w-full flex justify-end mb-3">
-                        <button onclick="showAddClient()" class="px-4 bg-[#833ae0] rounded-sm py-1 text-white flex items-center justify-center gap-2">
+                        {{-- <button onclick="showAddClient()" class="px-4 bg-[#833ae0] rounded-sm py-1 text-white flex items-center justify-center gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="14" height="14" fill="white"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
                             <p>Add new</p>
-                        </button>
+                        </button> --}}
                     </div>
 
                     <table class="w-full border-collapse mt-10">
                         <tr class="bg-[#F2EBFB] text-left">
-                            <th class="w-1/5 p-2">Parents Name</th>
                             <th class="w-1/5 py-2">Childs Name</th>
+                            <th class="w-1/5 py-2">Course</th>
                             <th class="w-[5%] py-2">Age</th>
                             <th class="w-[11%] py-2">Contact Number</th>
                             <th class="w-1/5 py-2">Email Address</th>
@@ -222,8 +229,8 @@
                         </script>
                         @foreach ($students as $student)
                             <tr class="border-b border-[#d8d8d8]">
-                                <td class="w-1/5 py-4 px-2"> {{$student->parent_name}} </td>
-                                <td class="w-1/5 py-2">{{$student->student_name}}</td>
+                                <td class="w-1/5 py-4">{{$student->student_name}}</td>
+                                <td class="w-1/5 py-2">Python Pro</td>
                                 <td class="w-[5%] py-2">{{$student->age}}</td>
                                 <td class="w-[11%] py-2">{{$student->contact_number}}</td>
                                 <td class="w-1/5 py-2">{{$student->email_address}}</td>
@@ -259,7 +266,7 @@
                                         <a href="#" onclick="editSchedule('{{ json_encode($student) }}')">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="13" height="13"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"/></svg>
                                         </a>
-                                        <a href="{{route('admin.delete_client', ['parent_name' => $student->parent_name])}}">
+                                        <a href="{{route('admin.delete_client', ['parent_name' => $student->id])}}">
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="13" height="13"><path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/></svg>
                                         </a>
                                     </div>
