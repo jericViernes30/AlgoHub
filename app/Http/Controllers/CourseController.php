9df0b9cfa37f071460c74161c26ee2188e19af85
Jeric James Viernes <jericviernes06@gmail.com>
2025-03-02 00:49:01 +0800

    added all the lessons

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Course;
+use App\Models\Lessons;
 use App\Models\SchedulesList;
 use Illuminate\Http\Request;
 
@@ -15,9 +16,14 @@ class CourseController extends Controller
 
     // Extract initials of the course name
     $courseName = $request->input('course_name');
-    $courseInitials = collect(explode(' ', $courseName))
-        ->map(fn($word) => strtoupper($word[0]))
-        ->join('');
+    if ($courseName === 'Unity Game Development') {
+        $courseInitials = 'UD'; // Special case
+    } else {
+        // General logic for extracting initials
+        $courseInitials = collect(explode(' ', $courseName))
+            ->map(fn($word) => strtoupper($word[0]))
+            ->join('');
+    }
 
     // Count occurrences of the course name in the database and increment by 1
     $existingCount = Course::where('course_name', $courseName)->count() + 1;
@@ -51,6 +57,44 @@ class CourseController extends Controller
 
     return redirect()->route('admin.schedule');
 }
+
+public function codingKnight(){
+    $lessons = Lessons::where('course', 'The Coding Knight')->get();
+    return view('Admin.subjects.coding_knight', ['lessons' => $lessons]);
+}
+public function visualProgramming(){
+    $lessons = Lessons::where('course', 'Visual Programming')->get();
+    return view('Admin.subjects.visual_programming', ['lessons' => $lessons]);
+}
+public function pythonStart1(){
+    $lessons = Lessons::where('course', 'Python Start')->where('year', 1)->get();
+    return view('Admin.subjects.python_start1', ['lessons' => $lessons]);
+}
+public function pythonStart2(){
+    $lessons = Lessons::where('course', 'Python Start')->where('year', 2)->get();
+    return view('Admin.subjects.python_start2', ['lessons' => $lessons]);
+}
+public function pythonPro1(){
+    $lessons = Lessons::where('course', 'Python Pro')->where('year', 1)->get();
+    return view('Admin.subjects.python_pro1', ['lessons' => $lessons]);
+}
+public function pythonPro2(){
+    $lessons = Lessons::where('course', 'Python Pro')->where('year', 2)->get();
+    return view('Admin.subjects.python_pro2', ['lessons' => $lessons]);
+}
+public function buildingWebsites(){
+    $lessons = Lessons::where('course', 'Building Websites')->get();
+    return view('Admin.subjects.building_websites', ['lessons' => $lessons]);
+}
+public function gameDesign(){
+    $lessons = Lessons::where('course', 'Game Design')->get();
+    return view('Admin.subjects.game_design', ['lessons' => $lessons]);
+}
+public function digitalLiteracy(){
+    $lessons = Lessons::where('course', 'Digital Literacy')->get();
+    return view('Admin.subjects.digital_literacy', ['lessons' => $lessons]);
+}
+
 
 
 }
