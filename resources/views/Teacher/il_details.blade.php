af7e8e8ffba78ec89d6425fb1af4986674d6744d
Jeric James Viernes <jericviernes06@gmail.com>
2025-03-02 00:57:01 +0800

    ui updates

--- resources/views/Teacher/il_details.blade.php
+++ resources/views/Teacher/il_details.blade.php
@@ -3,10 +3,12 @@
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <title>Introductory Class Details | AlgoHub</title>
+  <link rel="shortcut icon" href="{{asset('images/algo-logo.png')}}" type="image/x-icon">
   <script src="{{ asset('js/scripts.js') }}"></script>
   @vite('resources/css/app.css')
   <style>
@@ -43,38 +45,30 @@
 </head>
 <body class="bg-[#ececec]">
     <div class="w-full h-screen flex flex-col">
-        <div class="w-full bg-[#833ae0] flex py-2">
-            <div class="w-4/5 mx-auto flex justify-between">
-                <input type="search" name="search" placeholder="Search" class="px-2 py-1 rounded-md w-1/3">
-                <div class="flex items-center justify-center">
-                    <button class="rounded-md px-2 py-1 bg-[#F2EBFB] flex gap-1 items-center justify-center">
-                        <p>Hi, Teacher!</p>
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="12" height="12"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
-                        </span>
-                    </button>
+        <div class="w-full bg-[#632c7d] flex py-2">
+            <div class="w-11/12 mx-auto flex justify-end">
+                <div class="relative w-fit flex gap-4 items-center justify-center">
+                    <p class="text-sm font-medium text-white uppercase">{{$teacher->last_name}} {{$teacher->first_name}}</p>
                 </div>
             </div>
         </div>
         <div class="w-full flex h-screen bg-[#F2EBFB]">
             <div class="w-1/6 h-full bg-[#f9f9f9] text-sm">
-                <div class="w-full mx-auto flex gap-5 items-center mt-28">
-                    <a href="" class="text-[#48494b] px-5 hover:bg-[#F2EBFB] rounded-sm w-full py-2">Dashboard</a>
+                <div class="w-full mb-8 mt-10">
+                    <img src="https://lms.alg.academy/auth/v3/img/logo.d1092e37.svg" alt="Lesson Logo" class="w-3/4 block mx-auto">
                 </div>
-                <div>
-                    <div onclick="courseDropdown()">
-                        <div class="w-full flex items-center justify-around px-5 bg-[#F2EBFB] border-l-4 border-[#833ae0] relative hover:cursor-pointer">
-                            <p class=" w-full py-2">My Classes</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="12" height="12"><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
-                        </div>
-                        <div id="courses" class="hidden">
-                            <div class="w-full flex items-center px-10 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
-                                <a href="{{route('teacher.dashboard')}}" class="py-2">Regular Class</a>
-                            </div>
-                            <div class="w-full flex items-center px-10 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
-                                <a href="{{route('teacher.il_schedule')}}" class="py-2">Introductory Lesson</a>
-                            </div>
-                        </div>
+                <div class="">
+                    <div class="w-full flex items-center px-5 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
+                        <a href="{{route('teacher.dashboard')}}" class="py-2">Regular Class</a>
+                    </div>
+                    <div class="w-full flex items-center px-5 relative bg-[#F2EBFB] border-l-4 border-[#632c7d] hover:cursor-pointer">
+                        <a href="{{route('teacher.il_schedule')}}" class="py-2">Introductory Lesson</a>
+                    </div>
+                    <div class="w-full flex items-center px-5 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
+                        <a href="{{route('teacher.profile')}}" class="py-2">Profile</a>
+                    </div>
+                    <div class="w-full flex items-center px-5 relative hover:bg-[#F2EBFB] hover:cursor-pointer">
+                        <a href="{{route('teacher.logout')}}" class="py-2">Logout</a>
                     </div>
                 </div>
             </div>
@@ -112,7 +106,7 @@
                 </div>
 
                 <div class="w-full flex items-center">
-                    <p class="w-fit p-2 bg-white rounded-tr-md rounded-tl-md text-sm text-gray-700">Students</p>
+                    <p class="w-fit py-2 px-5 bg-white rounded-tr-md rounded-tl-md text-sm text-gray-700">Students</p>
                 </div>
                 <div class="w-full bg-white rounded-bl-lg rounded-br-lg rounded-tr-lg p-4">
                     <div class="flex items-center font-medium text-blue-950 text-sm mb-4 pt-5">
@@ -149,7 +143,7 @@
                                 </p>
                             </div>
                             <select name="actions" 
-                                    class="action-select outline-none border-2 rounded-md py-1 px-2 border-gray-400 focus:border-[#833ae0]
+                                    class="action-select outline-none border-2 rounded-md py-1 px-2 border-gray-400 focus:border-[#632c7d]
                                     @if ($student->status != 'Pending') cursor-not-allowed bg-gray-400 border-none @endif"
                                     @if ($student->status != 'Pending') disabled @endif
                             >
