31a42bd02e0fd3979d2386c438e67bd5f16c62a7
Jeric James Viernes <jericviernes06@gmail.com>
2025-03-02 00:51:42 +0800

    new table

---
+++ database/migrations/2025_02_23_105836_create_python_starts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('python_starts', function (Blueprint $table) {
+            $table->id();
+            $table->string('topic');
+            $table->string('code');
+            $table->string('lessons');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('python_starts');
+    }
+};
